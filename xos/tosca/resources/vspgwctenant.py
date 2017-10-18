de61ab27cde373525dbf13e5c0d243e05247369e
jianhaoch <itria50355@itri.org.tw>
2017-10-18 21:27:46 +0800

    fix tosca error

--- xos/tosca/resources/vspgwctenant.py
+++ xos/tosca/resources/vspgwctenant.py
@@ -25,16 +25,17 @@ class XOSVSPGWCTenant(XOSResource):
     def get_xos_args(self, throw_exception=True):
         args = super(XOSVSPGWCTenant, self).get_xos_args()
 
-        # ExampleTenant must always have a provider_service
         provider_name = self.get_requirement("tosca.relationships.TenantOfService", throw_exception=throw_exception)
         if provider_name:
-            args["provider_service"] = self.get_xos_object(Service, throw_exception=throw_exception, name=provider_name)
+            args["owner"] = self.get_xos_object(Service, throw_exception=throw_exception, name=provider_name)
 
         return args
 
     def get_existing_objs(self):
         args = self.get_xos_args(throw_exception=False)
-        return VSPGWCTenant.get_tenant_objects().filter(provider_service=args["provider_service"], service_specific_id=args["service_specific_id"])
+        owner = args.get("provider", None)
+        if owner:
+            return [ self.get_xos_object(owner=owner) ]
         return []
 
     def can_delete(self, obj):
